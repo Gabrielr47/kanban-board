f0edd13c2e010754b68acc8b06a189bd0e4c830b
Gabriel Rodrigues <gabrielr47@gmail.com>
2023-10-18 17:06:59 +0200

    add swimlanes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import kanban from './assets/kanban.json';
 import KanbanHeaderColumns from './components/KanbanHeaderColumns.vue';
+import KanbanSwimlanes from './components/KanbanSwimlanes.vue';
+
+const headerColumns = ref(kanban?.data?.columns);
+const swimlanes = ref(kanban?.data?.swimlanes);
 
 console.log(kanban);
 </script>
 
 <template>
   <div class="kanban">
-      <KanbanHeaderColumns :columns="kanban?.data?.columns"/>
+      <KanbanHeaderColumns :columns="headerColumns"/>
+      <KanbanSwimlanes :swimlanes="swimlanes"/>
   </div>
 </template>
---
+++ src/components/KanbanSwimlanes.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import draggable from 'vuedraggable'
+
+defineProps({
+  swimlanes: {
+    type: Array,
+    required: true
+  },
+});
+
+</script>
+
+<template>
+  <draggable
+    :list="swimlanes"
+    group="people"
+    item-key="key"
+    ghost-class="ghost"
+  >
+    <template #item="{ element }">
+      <div class="kanban-swimlane" :data-swimlane="element.key">
+        <div class="kanban-swimlane-header">
+          <p  class="handle" @click="element.isClosed = !element.isClosed">{{ element.label }}</p>
+          <div v-if="!element.isClosed">
+            Pipoca
+          </div>
+        </div>
+      </div>
+    </template>
+  </draggable>
+</template>
+<style scoped>
+.handle {
+  cursor: pointer;
+}
+</style>
