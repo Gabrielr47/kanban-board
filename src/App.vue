06ae347dff78d02a7168307a5412fb666822c7bf
Gabriel Rodrigues <gabrielr47@gmail.com>
2023-10-18 16:43:40 +0200

    add kanban header columns

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import kanban from './assets/kanban.json';
+import KanbanHeaderColumns from './components/KanbanHeaderColumns.vue';
 
+console.log(kanban);
 </script>
 
 <template>
-  <div> Kanban Board </div>
+  <div class="kanban">
+      <KanbanHeaderColumns :columns="kanban?.data?.columns"/>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/KanbanHeaderColumns.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import draggable from 'vuedraggable'
+
+const props = defineProps({
+  columns: {
+    type: Array,
+    required: true
+  },
+});
+
+const columnsLength = ref(props.columns?.length);
+
+</script>
+<template>
+  <draggable
+    :list="columns"
+    class="columns"
+    group="people"
+    item-key="key"
+    ghost-class="ghost"
+  >
+    <template #item="{ element }">
+      <div class="column" :data-header-column="element.key">
+        <div class="kanban-header">
+          <p>{{ element.label }}</p>
+        </div>
+      </div>
+    </template>
+  </draggable>
+</template>
+<style scoped>
+.columns {
+  display: grid;
+  grid-template-columns: repeat(v-bind(columnsLength), minmax(12rem, 20rem));
+  gap: 6px;
+}
+</style>
+```
